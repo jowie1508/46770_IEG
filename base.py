e8d813f6a728b93ca415bfe65bbcb27a11400e67
Jonas Wiendl <jonas.wiendl@posteo.de>
2025-04-24 13:33:14 +0200

    implemented small change to make codes compatible

--- base.py
+++ base.py
@@ -14,49 +14,38 @@ sys.path.append(project_root)  # Add project root to Python's search path
 def data_file(filename):
     return os.path.join(project_root, "data", filename)
 
-def build_network(solve=True, year =2015, countries=["DEU"], coordinates={'DEU': (51.15, 10.45)}):
-    global global_costs
-    
-    # Annuity Function
-    def annuity(r, n):
-        return r / (1 - 1 / (1 + r)**n) if r > 0 else 1 / n
+# Annuity Function
+def annuity(r, n):
+    return r / (1 - 1 / (1 + r)**n) if r > 0 else 1 / n
 
-    # Base Model
-    # 1. Create the network
-    
-    snapshots = pd.date_range(f"{year}-01-01", f"{year}-12-31 23:00", freq="H")
-    # 2. Set the modeling year
-    if len(snapshots) == 8784:
-        # Create a mask to exclude February 29
-        snapshots = snapshots[~((snapshots.month == 2) & (snapshots.day == 29))]
-    
-    network = pypsa.Network()
-    network.set_snapshots(snapshots)
+def load_hydro_data(country, network):
+    # Load and preprocess hydro inflow data
+    inflow_ror_GWh_day = pd.read_csv(data_file(f"Hydro_Inflow_{country[:2]}.csv"))
+    inflow_ror_GWh_day = inflow_ror_GWh_day[inflow_ror_GWh_day["Year"] == 2012]
+    inflow_ror_GWh_day["date"] = pd.to_datetime(inflow_ror_GWh_day[["Year", "Month", "Day"]])
+    inflow_ror_GWh_day.set_index("date", inplace=True)
 
-    # 3. Select country and Add bus
-    country = "DEU"  # Germany
-    network.add("Bus",
-        f"{country}_elec",
-        y = coordinates[country][0],
-        x = coordinates[country][1],
-        carrier="AC")
-    
+    ## convert to MW (average over day)
+    df_daily = inflow_ror_GWh_day[["Inflow [GWh]"]]
+    df_daily["Inflow [MW]"] = df_daily["Inflow [GWh]"] * 1000 / 24  # MW average per hour
+    ## Resample to hourly resolution (linear interpolation)
+    inflow_ror_hourly = df_daily["Inflow [MW]"].resample("h").interpolate("linear")
+    ## normalize
+    p_nom_ror = inflow_ror_hourly.max()
+    p_max_pu_ror = inflow_ror_hourly / p_nom_ror
+    ## adapt to snapshot format
+    p_max_pu_ror= p_max_pu_ror[:8760]
+    p_max_pu_ror.index = network.snapshots
 
-    # 5. Load demand time series
+    return p_max_pu_ror
 
 
+def load_time_series():
+    # demand time series
     df_elec = pd.read_csv(data_file("electricity_demand.csv"), sep=';', index_col=0) # in MWh
     df_elec.index = pd.to_datetime(df_elec.index) #change index to datatime
 
-
-    # 6. Add demand as a load to the bus
-    network.add("Load",
-                name=f"{country}_load",
-                bus=f"{country}_elec",
-                p_set=df_elec[country].values)
-
-    # 7. Importing capacity factor data
-
+    # capacity factors
     df_onshorewind = pd.read_csv(data_file('onshore_wind_1979-2017.csv'), sep=';', index_col=0)
     df_onshorewind.index = pd.to_datetime(df_onshorewind.index)
 
@@ -65,53 +54,17 @@ def build_network(solve=True, year =2015, countries=["DEU"], coordinates={'DEU':
 
     df_solar = pd.read_csv(data_file('pv_optimal.csv'), sep=';', index_col=0)
     df_solar.index = pd.to_datetime(df_solar.index)
+    
+    return df_elec, df_onshorewind, df_offhorewind, df_solar
 
-    # filter for capacity factor data for Germany
-    CF_onwind = df_onshorewind[country][[hour.strftime("%Y-%m-%dT%H:%M:%SZ") for hour in network.snapshots]]
-    CF_offwind = df_offhorewind[country][[hour.strftime("%Y-%m-%dT%H:%M:%SZ") for hour in network.snapshots]]
-    CF_solar = df_solar[country][[hour.strftime("%Y-%m-%dT%H:%M:%SZ") for hour in network.snapshots]]
 
-    carriers = [
-        "onwind",
-        "offwind",
-        "solar",
-        "OCGT",
-        "CCGT",
-        "hydro",
-        "ror",
-        "coal",
-        "lignite",
-        "biomass CHP",
-        "battery storage",
-    ]
-
-    # 8. Add hydro inflow data
-
-    # Load and preprocess hydro inflow data
-    inflow_ror_GWh_day = pd.read_csv(data_file(f"Hydro_Inflow_{country[:2]}.csv"))
-    inflow_ror_GWh_day = inflow_ror_GWh_day[inflow_ror_GWh_day["Year"] == 2012]
-    inflow_ror_GWh_day["date"] = pd.to_datetime(inflow_ror_GWh_day[["Year", "Month", "Day"]])
-    inflow_ror_GWh_day.set_index("date", inplace=True)
-
-    # convert to MW (average over day)
-    df_daily = inflow_ror_GWh_day[["Inflow [GWh]"]]
-    df_daily["Inflow [MW]"] = df_daily["Inflow [GWh]"] * 1000 / 24  # MW average per hour
-    # Resample to hourly resolution (linear interpolation)
-    inflow_ror_hourly = df_daily["Inflow [MW]"].resample("H").interpolate("linear")
-    # normalize
-    p_nom_ror = inflow_ror_hourly.max()
-    p_max_pu_ror = inflow_ror_hourly / p_nom_ror
-    # adapt to snapshot format
-
-    p_max_pu_ror= p_max_pu_ror[:8760]
-    p_max_pu_ror.index = network.snapshots
-    p_max_pu_ror.head()
-
-    # 9. Importing cost data
-    cost_year = 2025
+def load_technology_data(cost_year = 2025, technologies=["onwind", "offwind", "solar", "OCGT", "CCGT", "hydro", "ror", "coal", "lignite", "biomass CHP","battery storage",
+        "battery inverter"]):
+    
     url = f"https://raw.githubusercontent.com/PyPSA/technology-data/master/outputs/costs_{cost_year}.csv"
     costs = pd.read_csv(url, index_col=[0, 1])
 
+    # convert from kW to MW
     costs.loc[costs.unit.str.contains("/kW"), "value"] *= 1e3
     costs.unit = costs.unit.str.replace("/kW", "/MW")
 
@@ -140,30 +93,57 @@ def build_network(solve=True, year =2015, countries=["DEU"], coordinates={'DEU':
     annuity_results = costs.apply(lambda x: annuity(x["discount rate"], x["lifetime"]), axis=1)
     costs["capital_cost"] = (annuity_results + (costs["FOM"] / 100)) * costs["investment"]
 
-    costs = costs.loc[costs.index.get_level_values(0).isin(
-        ["onwind",
-        "offwind",
-        "solar",
-        "OCGT",
-        "CCGT",
-        "hydro",
-        "ror",
-        "coal",
-        "lignite",
-        "biomass CHP",
-        "battery storage",
-        "battery inverter"
-    ])]
-    global_costs = costs
-    # 10. Adding carriers
+    costs = costs.loc[costs.index.get_level_values(0).isin(technologies)]
+
+    return costs
+
+
+def build_network(solve=True, year =2015, carriers=["onwind","offwind","solar","OCGT","CCGT","hydro","ror","coal","lignite","biomass CHP",
+                                                    #"battery storage",
+                                                    ],
+                country = "DEU", countries=["DEU"], coordinates={'DEU': (51.15, 10.45)}):
+    
+    # 1. Create the network
+    network = pypsa.Network()
+    snapshots = pd.date_range(f"{year}-01-01", f"{year}-12-31 23:00", freq="h")
+    if len(snapshots) == 8784:
+        # Create a mask to exclude February 29
+        snapshots = snapshots[~((snapshots.month == 2) & (snapshots.day == 29))]
+    network.set_snapshots(snapshots)
+
+    # 2. Select country and Add bus
+    network.add("Bus",
+        f"{country}_elec",
+        y = coordinates[country][0],
+        x = coordinates[country][1],
+        carrier="AC")
+    
+
+    # 3. Load time series and technology data
+    df_elec, df_onshorewind, df_offhorewind, df_solar = load_time_series()
+    costs = load_technology_data()
+    p_max_pu_ror = load_hydro_data(country, network)
+
+    # 4. Add demand as a load to the bus
+    network.add("Load",
+                name=f"{country}_load",
+                bus=f"{country}_elec",
+                p_set=df_elec[country].values)
+    
+    # 5. Adding carriers
     network.add(
         "Carrier",
         carriers,
         co2_emissions=[costs.at[c, "CO2 intensity"] for c in carriers],
     )
 
-    # 11. Adding Generators
-    # Add "Dispatchable Generators"
+    # 6. Calculating capacity factor data
+    CF_onwind = df_onshorewind[country][[hour.strftime("%Y-%m-%dT%H:%M:%SZ") for hour in network.snapshots]]
+    CF_offwind = df_offhorewind[country][[hour.strftime("%Y-%m-%dT%H:%M:%SZ") for hour in network.snapshots]]
+    CF_solar = df_solar[country][[hour.strftime("%Y-%m-%dT%H:%M:%SZ") for hour in network.snapshots]]
+
+    # 7. Adding Generators
+    # Add "Dispatchable Generators" - limits related to resource scarcity
     network.add(
                 "Generator",
                 f"coal",
@@ -204,7 +184,7 @@ def build_network(solve=True, year =2015, countries=["DEU"], coordinates={'DEU':
             overwrite=True)
 
     # Peakers
-    # add OCGT (Open Cycle Gas Turbine) generator
+    # add OCGT (Open Cycle Gas Turbine) generator - limits related to resource scarcity 
     network.add("Generator",
                 f"OCGT",
                 bus=f"{country}_elec",
@@ -231,14 +211,14 @@ def build_network(solve=True, year =2015, countries=["DEU"], coordinates={'DEU':
                 overwrite=True      
             )
 
-    ## Variables
-    # add onshore wind generator
+    ## Variables - limits optional, in order to make the system feasible
+    # add onshore wind generator 
     network.add("Generator",
                 f"onwind",
                 bus=f"{country}_elec",
                 p_nom_extendable=True,
                 carrier="onwind",
-                p_nom_max=180000, # maximum capacity can be limited due to environmental constraints
+                #p_nom_max=180000, # maximum capacity can be limited due to environmental constraints
                 capital_cost = costs.at["onwind", "capital_cost"],
                 marginal_cost = costs.at["onwind", "marginal_cost"],
                 p_max_pu = CF_onwind.values,
@@ -250,7 +230,7 @@ def build_network(solve=True, year =2015, countries=["DEU"], coordinates={'DEU':
                 bus=f"{country}_elec",
                 p_nom_extendable=True,
                 carrier="offwind",
-                p_nom_max=75000, # maximum capacity can be limited due to environmental constraints
+                #p_nom_max=75000, # maximum capacity can be limited due to environmental constraints
                 capital_cost = costs.at["offwind", "capital_cost"],
                 marginal_cost = costs.at["offwind", "marginal_cost"],
                 p_max_pu = CF_offwind.values,
@@ -262,69 +242,11 @@ def build_network(solve=True, year =2015, countries=["DEU"], coordinates={'DEU':
                 bus=f"{country}_elec",
                 p_nom_extendable=True,
                 carrier="solar",
-                p_nom_max=385000, # maximum capacity can be limited due to environmental constraints
+                #p_nom_max=385000, # maximum capacity can be limited due to environmental constraints
                 capital_cost = costs.at["solar", "capital_cost"],
                 marginal_cost = costs.at["solar", "marginal_cost"],
                 p_max_pu = CF_solar.values,
                 overwrite=True)
-    
-
-    if len(countries) > 1:
-        for country in countries[1:]:
-            # add demand 
-            network.add("Bus",
-                        f"{country}_elec",
-                        y = coordinates[country][0],
-                        x = coordinates[country][1],
-                        carrier="AC")
-            network.add("Load", 
-                        f"{country}_load",
-                        bus=f"{country}_elec",
-                        p_set=df_elec[country].values,
-                        carrier = "AC")
-            network.add(
-                "Generator",
-                f"{country} OCGT",
-                bus=f"{country}_elec",
-                carrier="OCGT",
-                capital_cost=costs.at["OCGT", "capital_cost"],
-                marginal_cost=costs.at["OCGT", "marginal_cost"],
-                efficiency=costs.at["OCGT", "efficiency"],
-                p_nom_extendable=True,
-            )   
-            CF_wind = df_onshorewind[country][[hour.strftime("%Y-%m-%dT%H:%M:%SZ") for hour in network.snapshots]]
-            network.add("Generator",
-                f"{country}_onwind",
-                bus=f"{country}_elec",
-                carrier="onwind",
-                capital_cost=costs.at["onwind", "capital_cost"],
-                marginal_cost=costs.at["onwind", "marginal_cost"],
-                p_max_pu=CF_wind.values,
-                efficiency=costs.at["onwind", "efficiency"],
-                p_nom_extendable=True,
-            )
-            CF_solar = df_solar[country][[hour.strftime("%Y-%m-%dT%H:%M:%SZ") for hour in network.snapshots]]
-            network.add("Generator",
-                f"{country}_solar",
-                bus=f"{country}_elec",
-                carrier="solar",
-                capital_cost=costs.at["solar", "capital_cost"],
-                marginal_cost=costs.at["solar", "marginal_cost"],
-                p_max_pu=CF_solar.values,
-                efficiency=costs.at["solar", "efficiency"],
-                p_nom_extendable=True,
-            )
-            network.add(
-                "Line",
-                f"DEU-{country}",
-                bus0="DEU_elec",
-                bus1=f"{country}_elec",
-                s_nom = 1000,
-                x = 1,
-                r = 1,
-            )
-
-
 
     if solve:
         network.optimize(solver_name="highs")
